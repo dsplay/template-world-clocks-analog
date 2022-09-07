e555356ebb5958cba6108fb636d9bff15980f3c2
Paulo Coutinho <pcmnac@gmail.com>
2022-09-07 09:59:11 +0100

    ⬆️ upgrading deps

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,7 @@ import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import registerServiceWorker from './registerServiceWorker';
 import './index.sass';
 import './fonts.sass';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
